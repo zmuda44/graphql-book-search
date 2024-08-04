36c0ee29c871a357046cc1b9b38ab8b5195aad74
Greg Bailey <gregbailey617@gmail.com>
2024-08-04 18:21:41 -0400

    adding code to front end to run graph ql. added dependencies and started server on app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,18 @@ import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
+    </ApolloProvider>
     </>
   );
 }
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,6 +24,14 @@ const typeDefs = `
 
 module.exports = typeDefs;
 
+// type Mutation {
+//   addThought(thoughtText: String!, thoughtAuthor: String!): Thought
+//   addComment(thoughtId: ID!, commentText: String!): Thought
+//   removeThought(thoughtId: ID!): Thought
+//   removeComment(thoughtId: ID!, commentId: ID!): Thought
+// }
+// `;
+
 
 
 
